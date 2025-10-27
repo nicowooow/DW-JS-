b47c8be49a3a6a5ce992e1b6d03be2ca6c639645
Nico-hi <nicolasgianfranco123@gmail.com>
2025-10-27 18:46:21 +0100

    para la parte de animar una bola

---
+++ bola_rebote/index.js
@@ -0,0 +1,5 @@
+let height= parseInt(window.getComputedStyle(ball).getPropertyValue("top"));
+let width =  parseInt(window.getComputedStyle(ball).getPropertyValue("left"));
+
+let ball = document.querySelector('.ball');
+
